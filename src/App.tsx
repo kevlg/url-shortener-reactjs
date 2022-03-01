2b95d4dba913da5267dfa6b34a4b2a81eeab6191
Kevin Lopez <g.lopezkevin@gmail.com>
2022-02-28 22:46:45 -0600

    Add materialui

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import * as React from 'react';
-import './App.css';
-
 
 class App extends React.Component<{}, {}> {
 	render() {
 
 		return ( 
-			<h2>URL shortener</h2>			
+			<h2>URL shortener</h2>		
 		);
 	}
   
