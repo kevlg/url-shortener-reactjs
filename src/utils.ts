ffe985492140d6e9d2c5ccf6440c66f6cf9a895a
Kevin Lopez <g.lopezkevin@gmail.com>
2022-03-02 11:34:11 -0600

    add environment variable

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: 'http://localhost:3030/',
+    baseURL: process.env.REACT_APP_SERVER,
     timeout: 1000,
 });
 
